fbccbba3437eec75892e8a7f2c6b5522a3e1160c
Abdullah1dev <242677@students.au.edu.pk>
2026-07-26 11:24:05 +0500

    Create user database model

---
+++ app/features/users/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.infrastructure.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(String(100))
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String(255)
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
