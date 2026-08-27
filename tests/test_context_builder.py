16792fbee3f93b88bf7b1324ab4b6d0ded3c33a7
Abdullah Tahir <242677@students.au.edu.pk>
2026-08-28 01:15:37 +0500

    try to test context builder seprately but the test got failed.

---
+++ tests/test_context_builder.py
@@ -0,0 +1,32 @@
+from app.infrastructure.database.config import SessionLocal
+from app.infrastructure.embeddings.service import EmbeddingService
+from app.infrastructure.vector_search.service import VectorSearchService
+from app.infrastructure.rag.context_builder import RAGContextBuilder
+
+
+db = SessionLocal()
+
+try:
+
+    embedding_service = EmbeddingService()
+
+    vector_search = VectorSearchService(
+        embedding_service=embedding_service
+    )
+
+    chunks = vector_search.search(
+        db=db,
+        query="What is this PDF about?",
+        top_k=2,
+    )
+
+    context_builder = RAGContextBuilder()
+
+    context = context_builder.build_context(chunks)
+
+    print("\n========== RAG CONTEXT ==========\n")
+    print(context)
+    print("\n=================================\n")
+
+finally:
+    db.close()
